626c1c1061f01c9f3ded5f69bac30140ddb22fd1
Weverton dos Santos <wever-santos@hotmail.com>
2023-05-03 11:00:04 -0300

    this

--- src/conta.php
+++ src/conta.php
@@ -5,6 +5,15 @@ class Conta
     public $cpfTitular;
     public $nomeTitular;
     public $saldo;
+
+    public function sacarSaldo(float $valorASacar)
+    {
+        if($valorASacar > $this->saldo){
+            echo 'Você não pode sacar esse valor' . PHP_EOL;
+        }else{
+            $this->saldo -= $valorASacar;
+        }
+    }
 }
 
 $primeiraConta = new Conta();
