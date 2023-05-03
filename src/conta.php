766adbbcd1b9b01370c0e8275e62b845ab58d4bb
Weverton dos Santos <wever-santos@hotmail.com>
2023-05-03 09:33:41 -0300

    class

--- src/conta.php
+++ src/conta.php
@@ -1,14 +1,8 @@
 <?php
 
-function criarConta(string $cpf, string $nomeTitular, float $saldo) : array{
-    return [
-        $cpf => [
-            'titular' => $nomeTitular,
-            'saldo' => $saldo,
-        ]
-    ];
-};
-
-$conta = criarConta('123.456.789-10', 'Weverton', 500);
-
-var_dump($conta);
+class Conta
+{
+    public string $cpfTitular;
+    public string $nomeTitular;
+    public string $saldo;
+}
